65a4076ccc8d0a7275ede7168471b8896c1a6015
capnolove <insocdola@gmail.com>
2025-10-22 18:45:44 +0800

    login signup and front page

--- app/page.tsx
+++ app/page.tsx
@@ -2,102 +2,33 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="flex min-h-screen flex-col p-5 bg-[#620089]">
+      <div className="flex h-10 shrink-0 items-end rounded-lg bg-[#D8AAEA] px-15 py-6 md:h-20 font-instrument-sans text-[18px] font-semibold text-black">
+        <p>MySimFisher</p>
+        <div className="ml-2 p-0">
+          <Image src="/logo.png" alt="Logo" width={34} height={34} className="rounded-sm" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="flex items-center gap-10 ml-auto">
+          <button className="px-3 py-1 text-sm font-instrument-sans text-[18px] font-semibold text-black rounded-md bg-white hover:bg-[#C8C8C8]">
+            Sign up
+          </button>
+          <button className="px-3 py-1 text-sm font-instrument-sans text-[18px] font-semibold text-black rounded-md bg-white hover:bg-[#C8C8C8]">
+            Login
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-1 flex-row w-full bg-transparent">
+        <div className="flex flex-1 flex-col mt-6 h-1/2 w-1/3 shrink-0 items-center justify-center rounded-lg bg-[#D8AAEA] md:h-20 font-instrument-sans font-semibold text-black">
+          <p className="m-0 justify-center p-4 text-left text-[26px] text-[#583535]">Start building your “last line of defense” today</p>
+          <p className="m-0 justify-center p-4 text-left text-[18px] text-[#583535]">Launching hyper-realistic phishing campaigns to form your “last line of defense”. Get started to begin a new security training program for your corporation</p>
+            <button className="px-6 py-3 text-lg font-instrument-sans text-[18px] font-semibold text-black rounded-md bg-white hover:bg-[#C8C8C8] min-w-[240px]">
+            Get started now
+            </button>
+        </div>
+        <div className="w-full md:w-1/3 mt-6 md:mt-0 h-1/2 ml-auto relative rounded-lg bg-transparent">
+          <Image src="/logo.png" alt="Logo" fill className="object-contain" />
+        </div>
+      </div>
+    </main>
   );
 }
